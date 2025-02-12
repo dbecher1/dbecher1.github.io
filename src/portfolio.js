ed51c4571eae1b123f6d60d1d037f7d71f6917a3
dbecher <debecher93@gmail.com>
2025-02-12 13:40:19 -0500

    Testing actions deployment

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Daniel Becher",
-  title: "Hi all, I'm Dan",
+  title: "Testing deployment",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
