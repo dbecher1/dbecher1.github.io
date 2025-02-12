d6e5531eaff0f551f61f3337137f89f07b09aa15
dbecher <debecher93@gmail.com>
2025-02-12 13:52:57 -0500

    Updated .prettierrc to match personal prefs

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -1,23 +1,23 @@
-import React, {Component, Suspense} from "react";
-import Lottie from "lottie-react";
-import Loading from "../../containers/loading/Loading";
+import React, { Component, Suspense } from 'react';
+import Lottie from 'lottie-react';
+import Loading from '../../containers/loading/Loading';
 
 export default class DisplayLottie extends Component {
-  render() {
-    const animationData = this.props.animationData;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData
-    };
+	render() {
+		const animationData = this.props.animationData;
+		const defaultOptions = {
+			loop: true,
+			autoplay: true,
+			animationData: animationData,
+		};
 
-    return (
-      <Suspense fallback={<Loading />}>
-        <Lottie
-          animationData={defaultOptions.animationData}
-          loop={defaultOptions.loop}
-        />
-      </Suspense>
-    );
-  }
+		return (
+			<Suspense fallback={<Loading />}>
+				<Lottie
+					animationData={defaultOptions.animationData}
+					loop={defaultOptions.loop}
+				/>
+			</Suspense>
+		);
+	}
 }
